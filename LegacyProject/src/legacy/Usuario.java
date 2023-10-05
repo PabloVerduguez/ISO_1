e93e0d16597809249bc50c3ac8ec3b1a11fa10fb
Pablo Verdúguez <Pabloverduguez913@gmail.com>
2023-10-05 10:27:20 +0200

    Codigo Funcional

--- LegacyProject/src/legacy/Usuario.java
+++ LegacyProject/src/legacy/Usuario.java
@@ -16,19 +16,19 @@ public class Usuario {
 	public String mPassword;
 	
 	
-	//Constructor para la creaci—n de un objeto Usuario vacio
+	//Constructor para la creaciï¿½n de un objeto Usuario vacio
 	public Usuario(){
 		this.mLogin = null;
 		this.mPassword = null;
 	}
 	
-	//Constructor para la creaci—n de un Usuario
+	//Constructor para la creaciï¿½n de un Usuario
 	public Usuario(String login, String password){
 		this.mLogin = login;
 		this.mPassword = password;
 	}
 	
-	//Selecci—n de un usuario de la base de datos a partir del login y el password
+	//Selecciï¿½n de un usuario de la base de datos a partir del login y el password
 	@SuppressWarnings("unchecked")
 	public static Usuario read(String login, String password) throws Exception{
 		String l,g;
@@ -57,7 +57,7 @@ public class Usuario {
 		return u;
 	}
 	
-	//Inserci—n de un nuevo usuario en la base de datos
+	//Inserciï¿½n de un nuevo usuario en la base de datos
 	public int insert() throws Exception{
 		Driver derbyEmbeddedDriver = new EmbeddedDriver();
 		DriverManager.registerDriver(derbyEmbeddedDriver);
@@ -66,6 +66,30 @@ public class Usuario {
     	int res=stmt.executeUpdate();
     	stmt.close();
     	mBD.close();
+		return res;
+	}
+
+	// public int delete() throws Exception{
+	// 	Driver derbyEmbeddedDriver = new EmbeddedDriver();
+	// 	DriverManager.registerDriver(derbyEmbeddedDriver);
+	// 	Connection mBD = DriverManager.getConnection(""+BDConstantes.DRIVER+":"+BDConstantes.DBNAME+";create=false", BDConstantes.DBUSER, BDConstantes.DBPASS);
+	// 	PreparedStatement stmt = mBD.prepareStatement("DELETE FROM Usuario WHERE login=" + this.mLogin + " AND pass=" + this.mPassword);
+    // 	int res=stmt.executeUpdate();
+    // 	stmt.close();
+    // 	mBD.close();
+	// 	return res;
+	// }
+
+	public int delete() throws Exception{
+		Driver derbyEmbeddedDriver = new EmbeddedDriver();
+		DriverManager.registerDriver(derbyEmbeddedDriver);
+		Connection mBD = DriverManager.getConnection(""+BDConstantes.DRIVER+":"+BDConstantes.DBNAME+";create=false", BDConstantes.DBUSER, BDConstantes.DBPASS);
+		PreparedStatement stmt = mBD.prepareStatement("DELETE FROM Usuario WHERE login= ? AND pass= ?");
+		stmt.setString(1, this.mLogin);
+		stmt.setString(2, this.mPassword);
+		int res=stmt.executeUpdate();
+		stmt.close();
+		mBD.close();
 		return res;
 	}
 	
